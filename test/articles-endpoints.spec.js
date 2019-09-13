202ff0957a0ad61e918a22b44d27bfba74b4121c
Allan Cheng <allanhfcheng@gmail.com>
2019-09-13 01:18:30 -0700

    timestamp again

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -92,7 +92,8 @@ describe(`GET /articles/:article_id`, () => {
 })
 
 describe.only(`POST /articles`, () => {
-     it(`creates an article, responding with 201 and the new article`,  function() {
+  it(`creates an article, responding with 201 and the new article`, function() {
+        this.retries(3)
       const newArticle = {
                title: 'Test new article',
                style: 'Listicle',
