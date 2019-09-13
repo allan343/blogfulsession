31d75affb29b312734867be7bd581cd375590ced
Allan Cheng <allanhfcheng@gmail.com>
2019-09-13 01:37:59 -0700

    refractor

--- src/app.js
+++ src/app.js
@@ -4,74 +4,19 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const ArticlesService = require('./articles-service')
+const articlesRouter = require('./articles/articles-router')
 
 const app = express()
-const jsonParser = express.json()
 
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common'))
 app.use(cors())
 app.use(helmet())
 
-app.get('/articles', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getAllArticles(knexInstance)
-    .then(articles => {
-      res.json(articles.map(article => ({
-                 id: article.id,
-                 title: article.title,
-                 style: article.style,
-                 content: article.content,
-                 date_published: new Date(article.date_published),
-               })))
-    })
-    .catch(next)
-})
-
-app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-      if (!article) {
-        return res.status(404).json({
-          error: { message: `Article doesn't exist` }
-        })
-      }
-      res.json(article)
-    })
-    .catch(next)
-})
+app.use('/articles', articlesRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: 'Server error' }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
-
-app.post('/articles', jsonParser, (req, res, next) => {
-  
-    const { title, content, style } = req.body
-   const newArticle = { title, content, style }
-   ArticlesService.insertArticle(
-     req.app.get('db'),
-     newArticle
-   )
-     .then(article => {
-       res
-         .status(201)
-         .location(`/articles/${article.id}`)
-         .json(article)
-     })
-     .catch(next)
-    })
 
 module.exports = app
--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -3,7 +3,7 @@ const app = require('../src/app')
 const { makeArticlesArray } = require('./articles.fixtures')
 
 
-describe.only('Articles Endpoints', function() {
+describe('Articles Endpoints', function() {
   let db
 
   before('make knex instance', () => {
@@ -91,7 +91,7 @@ describe(`GET /articles/:article_id`, () => {
   })
 })
 
-describe.only(`POST /articles`, () => {
+describe(`POST /articles`, () => {
   it(`creates an article, responding with 201 and the new article`, function() {
         this.retries(3)
       const newArticle = {
