717771537f50b190492d27ec7715704434e91a57
Allan Cheng <allanhfcheng@gmail.com>
2019-09-13 02:07:08 -0700

    sanitizing

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -88,6 +88,31 @@ describe(`GET /articles/:article_id`, () => {
         .get(`/articles/${articleId}`)
         .expect(200, expectedArticle)
     })
+
+    context(`Given an XSS attack article`, () => {
+          const maliciousArticle = {
+             id: 911,
+           title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+             style: 'How-to',
+             content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`
+           }
+      
+           beforeEach('insert malicious article', () => {
+             return db
+               .into('blogful_articles')
+               .insert([ maliciousArticle ])
+           })
+      
+           it('removes XSS attack content', () => {
+             return supertest(app)
+               .get(`/articles/${maliciousArticle.id}`)
+               .expect(200)
+               .expect(res => {
+                 expect(res.body.title).to.eql('Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;')
+                 expect(res.body.content).to.eql(`Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`)
+               })
+           })
+         })
   })
 })
 
